66a706a7c54cf076565c86364679e0d353e60708
Alisher <alisherxujanov163@gmail.com>
2020-12-09 13:54:16 +0300

    for uploading to GIThub

--- Game/urls.py
+++ Game/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('', GameView.as_view(), name='quiz'),
 
     path('Vue/', quizInVue.as_view(), name='vue_django'),
-    # path('Vue/<str:id>/complete/', TaskComplete.as_view(), name='TaskComplete')
 ]
--- Game/views.py
+++ Game/views.py
@@ -25,27 +25,14 @@ class GameView(TemplateView):
 
 
 class quizInVue(ListView):
-    def get(self, request):
-        tasks = list(Item.objects.values())
+    model = Choice
+    template_name = 'game/quizInVue.html'
 
-        if request.is_ajax():
-            return JsonResponse({'tasks': tasks}, status=200)
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['queryset1'] = Choice.objects.all()
+        context_data['queryset2'] = Timer.objects.all().order_by(
+            '-started_time')
+        return context_data
 
-        return render(request, 'game/quizInVue.html')
-
-#     def post(self, request):
-#         bound_form = QuizForm(request.POST)
-
-#         if bound_form.is_valid():
-#             new_task = bound_form.save()
-#             return JsonResponse({'task': model_to_dict(new_task)}, status=200)
-
-#         return redirect('vue_django')
-
-
-# class TaskComplete(ListView):
-#     def post(self, request, id):
-#         task = Choice.objects.get(id=id)
-#         task.completed = True
-#         task.save()
-#         return JsonResponse({'task': model_to_dict(task)}, status=200)
+    # def quiz(self, request):
